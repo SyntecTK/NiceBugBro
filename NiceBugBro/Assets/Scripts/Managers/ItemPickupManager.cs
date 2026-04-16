e829518161b38239bb7a0dbb18a63e3106003e9f
LarsPenni <51168701+LarsPenni@users.noreply.github.com>
2026-04-16 13:56:53 +0200

    forgot to actually push that shit

---
+++ NiceBugBro/Assets/Scripts/Managers/ItemPickupManager.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickupManager : MonoBehaviour
+{
+    public static ItemPickupManager Instance { get; private set; }
+    
+    [SerializeField] private List<GameObject> spawnPoints;
+    [SerializeField] private GameObject pickupPrefab;
+
+    private readonly float pickupCooldown = 5f;
+    private float currentPickupCooldown;
+    private bool pickupCurrentlyReady;
+
+    private void Start()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        ResetPickupCooldown();
+    }
+
+    private void SpawnNewPickup()
+    {
+        GameObject newPickup = spawnPoints[Random.Range(0, spawnPoints.Count)];
+        Instantiate(pickupPrefab, newPickup.transform.position, newPickup.transform.rotation);
+        pickupCurrentlyReady = true;
+    }
+
+    private void OnEnable()
+    {
+        EventManager.CollectedUpgrade += UpgradeCollected;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.CollectedUpgrade -= UpgradeCollected;
+    }
+
+    private void UpgradeCollected()
+    {
+        ResetPickupCooldown();
+    }
+
+    private void Update()
+    {
+        if (!pickupCurrentlyReady)
+        {
+            currentPickupCooldown -= Time.deltaTime;
+            if (currentPickupCooldown <= 0) SpawnNewPickup();
+        }
+    }
+
+    private void ResetPickupCooldown()
+    {
+        pickupCurrentlyReady = false;
+        currentPickupCooldown = pickupCooldown;
+    }
+}
