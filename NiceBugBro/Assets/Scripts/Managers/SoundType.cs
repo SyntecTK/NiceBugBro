5bb61681c9aa87ac1e812e23bd95a7ccb2a1e7e3
Kaleid <50679111+Kaleid07@users.noreply.github.com>
2026-04-17 16:44:14 +0200

    add audio manager and some sounds

---
+++ NiceBugBro/Assets/Scripts/Managers/SoundType.cs
@@ -0,0 +1,14 @@
+public enum SoundType
+{
+    Footstep,
+    GunShot,
+    EnemyKill,
+    PlayerDamageTaken,
+    Jump,
+    Landing,
+    GameOver,
+    PowerUp,
+    EnemyShot,
+    DryFire,
+    Ricochete
+}
